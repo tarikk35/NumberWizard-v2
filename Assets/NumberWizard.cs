30aa1b58f2ae6e6c8b55872dc0d1c449a74828fd
Tarık Köprülü <tarikk3545@gmail.com>
2018-10-02 19:15:22 +0300

    NumberWizard2

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -4,38 +4,73 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
     int max, min,oncekiTahmin,tahmin,tahminSayisi;
-    enum oyunDurumu
+    bool maxSecildi=false, minSecildi=false;
+
+    enum oyunDurumlari
     {
         Baslamadi,
         Basladi,
         Hileci
     };
-    oyunDurumu durum = oyunDurumu.Baslamadi;
+    oyunDurumlari oyunDurumu = oyunDurumlari.Baslamadi;
 
 	// Use this for initialization
 	void Start () {
-
-        max = 1000;
-        min = 1;
-        tahmin = 500;
         tahminSayisi = 0;
         
         Debug.Log("Sayi sihirbazina hosgeldin .");
-        Debug.Log("Aklından bir sayı tut , ama bana söyleme .");
-        Debug.Log("Tuttuğun sayı "+min+" ile "+max+" arasında olsun .");
-        Debug.Log("Hazır olduğunda klavyenden ENTER tuşuna bas. ");
+        Debug.Log("Bu oyunda senden belli bir aralıkta bir sayı tutmanı isteyeceğim ve bu sayıyı bilmeye çalışacağım .");
+        Debug.Log("Ama önce sayı aralığının en küçük sayısını klavyenden girip ENTER tuşuna bas .");
 	}
+
+    void SayiyiBelirle(string sayi,bool minMi)
+    {
+        if(minMi)
+        {
+            string tempMin = min.ToString();
+            tempMin += sayi;
+            min = int.Parse(tempMin);
+            Debug.Log(min);
+        }
+        else
+        {
+            string tempMax = max.ToString();
+            tempMax += sayi;
+            max = int.Parse(tempMax);
+            Debug.Log(max);
+        }
+    }
+    
+    void SonHaneyiSil(bool minMi)
+    {
+        if (minMi)
+        {
+            min /= 10;
+            Debug.Log(min);
+        }
+        else
+        {
+            max /= 10;
+            Debug.Log(max);
+        }
+    }
+
+    int RastgeleTahmin()
+    {
+        return Random.Range(min, max);
+    }
+
 	void SonrakiTahmin()
     {
-        tahmin = (max + min) / 2;
+        tahmin = RastgeleTahmin();
         Debug.Log("Tuttuğun sayı " + tahmin + "den büyükse YUKARI OK, küçükse AŞAĞI OK tuşuna bas. Tahminim doğruysa ENTER tuşuna bas .");
         tahminSayisi++;
     }
 
 	// Update is called once per frame
 	void Update () {
-        
-        if (durum == oyunDurumu.Basladi&&Input.anyKeyDown)
+
+        if (oyunDurumu == oyunDurumlari.Basladi&&Input.anyKeyDown)
         {
             if(Input.GetKeyDown(KeyCode.UpArrow))
             {
@@ -51,23 +86,58 @@ public class NumberWizard : MonoBehaviour {
             }
             else if(Input.GetKeyDown(KeyCode.Return))
             {
-                Debug.Log("Tuttuğun sayıyı bildim !");
+                Debug.Log("Tuttuğun sayıyı "+tahminSayisi+" denemede bildim !");
                 UnityEditor.EditorApplication.isPlaying = false;
             }
-            if (oncekiTahmin == (min + max) / 2)
-                durum=oyunDurumu.Hileci;              
+            if (oncekiTahmin == min&&oncekiTahmin==max)
+                oyunDurumu=oyunDurumlari.Hileci;              
         }
-        else if (durum == oyunDurumu.Hileci)
+        else if (oyunDurumu == oyunDurumlari.Hileci)
         {
             Debug.Log("HİLECİ !");
         }
-        else if (Input.GetKeyDown(KeyCode.Return) && durum==oyunDurumu.Baslamadi)
-        {
-            Debug.Log("Demek hazırsın . ");
-            SonrakiTahmin();
-            durum = oyunDurumu.Basladi;
-        }
-            
 
+        if (Input.anyKeyDown&&oyunDurumu==oyunDurumlari.Baslamadi)
+        {
+            string key = Input.inputString;
+            int x;
+            bool isNumeric = int.TryParse(key,out x);
+
+            if (isNumeric && (!minSecildi||!maxSecildi))
+            {
+                SayiyiBelirle(key, !minSecildi);
+            }
+
+            else if (Input.GetKeyDown(KeyCode.Return))
+            {
+                if(!minSecildi)
+                {
+                    Debug.Log("Minimum sayıyı seçtiniz.");
+                    Debug.Log("Şimdi de maksimum sayıyı giriniz.");
+                    minSecildi = true;
+                }
+                else if(!maxSecildi)
+                {
+                    if(min>max)
+                    {
+                        Debug.Log("Minimum sayınız maksimum sayınızdan büyük olmalı . Lütfen sayınızı tekrar yazınız ");
+                        max = 0;
+                    }
+                    else
+                    {
+                        Debug.Log("Maksimum sayıyı seçtiniz. Tahminlerime başlıyorum.");
+                        maxSecildi = true;
+                        oyunDurumu = oyunDurumlari.Basladi;
+                        SonrakiTahmin();
+                    }                   
+                }               
+            }
+                
+            else if (Input.GetKeyDown(KeyCode.Backspace))
+            {
+                SonHaneyiSil(!minSecildi);
+            }
+               
+        }
 	}
 }
